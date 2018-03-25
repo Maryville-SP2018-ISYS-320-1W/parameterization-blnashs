96183020d6bba2a29219445af7c37fd0e4c06fdb
Barbara Nash <bnash1@live.maryville.edu>
2018-03-25 14:59:57 -0500

    mystery numbers

--- src/P1_MysteryNums.java
+++ src/P1_MysteryNums.java
@@ -6,12 +6,15 @@
 
 /*
  1. Your output prediction:
+ // 15, 42
+ // 10, 15 + 10
   
   
  */
 
 /*
  3. Were you correct? Explain if you were not (what did you learn?)
+ No, I was expecting the sentence  to return with values of x and y
  
  
  */
